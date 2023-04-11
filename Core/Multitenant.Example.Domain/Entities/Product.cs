68bd57dcb8fd3f6d5cd068323068f2b6993bd286
Cem <cem.net@hotmail.com>
2023-04-11 10:49:05 +0300

    Add project files.

---
+++ Core/Multitenant.Example.Domain/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using Multitenant.Example.Domain.Contracts;
+using Multitenant.Example.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multitenant.Example.Domain.Entities
+{
+    public class Product : BaseEntity, IMustHaveTenant
+    {
+        public Product() { }
+        public Product(string name, string description, int rate, int sed)
+        {
+            Name = name;
+            Description = description;
+            Rate = rate;
+            Sed = sed;
+        }
+
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public int Rate { get; private set; }
+        public int Sed { get; private set; }
+        public string TenantId { get; set; }
+    }
+}
---
+++ Infrastructure/Multitenant.Example.Persistence/Migrations/20230409211714_sel1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Multitenant.Example.Persistence.Migrations
+{
+    public partial class sel1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Sed",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sed",
+                table: "Products");
+        }
+    }
+}
